dd25a245b319fe9c9d3558638ee0e1acf161be5e
devilmasterz <131021692+devilmasterz@users.noreply.github.com>
2023-05-09 18:10:04 +0700

    Update database.sql

--- lib/database.sql
+++ lib/database.sql
@@ -31,7 +31,145 @@ ADD CONSTRAINT fk_hs
 FOREIGN KEY (MaHS)
 REFERENCES HOCSINH(MaHS);
 
--- bảng học sinh 
+
+-===========================================================================================
+
+create database englishcenter
+
+--admim
+
+CREATE TABLE admin 
+(UserName VARCHAR( 30) NOT NULL , 
+Password VARCHAR(30) NOT NULL , 
+PRIMARY KEY (UserName))
+
+
+--hocsinh
+
+CREATE TABLE hocsinh 
+(MaHS INT NOT NULL AUTO_INCREMENT,
+TenHS VARCHAR(30) NOT NULL , 
+GioiTinh VARCHAR(5) NOT NULL , 
+NgaySinh DATE NOT NULL , 
+Tuoi INT NOT NULL , 
+DiaChi VARCHAR(100) NOT NULL , 
+SDT VARCHAR(11) NULL , 
+Email VARCHAR(50) NULL , 
+PRIMARY KEY (MaHS)) 
+
+--tk_hs
+CREATE TABLE tk_hs 
+(UserName VARCHAR(30) NOT NULL , 
+Password VARCHAR(30) NOT NULL , 
+MaHS int,
+foreign key (MaHS) references hocsinh(MaHS) , 
+PRIMARY KEY (UserName)) 
+
+--phuhuynh
+CREATE TABLE phuhuynh 
+(MaPH INT NOT NULL AUTO_INCREMENT,
+TenPH VARCHAR(30) NOT NULL , 
+GioiTinh VARCHAR(5) NOT NULL , 
+NgaySinh DATE NOT NULL , 
+Tuoi INT NOT NULL , 
+DiaChi VARCHAR(100) NOT NULL , 
+SDT VARCHAR(11) NULL , 
+Email VARCHAR(50) NULL , 
+PRIMARY KEY (MaPH)) 
+
+--tk_ph
+CREATE TABLE tk_ph 
+(UserName VARCHAR(30) NOT NULL , 
+Password VARCHAR(30) NOT NULL , 
+MaPH int,
+foreign key (MaPH) references phuhuynh(MaPH) , 
+PRIMARY KEY (UserName)) 
+
+--ph-hs
+CREATE TABLE ph_hs
+(MAHS int,
+MaPH int,
+foreign key (MaPH) references phuhuynh(MaPH) ,
+foreign key (MaHS) references hocsinh(MaHS) ,
+PRIMARY KEY (MaHS,MaPH)) 
+
+--giaovien
+
+CREATE TABLE giaovien 
+(MaGV INT NOT NULL AUTO_INCREMENT,
+TenGV VARCHAR(30) NOT NULL , 
+GioiTinh VARCHAR(5) NOT NULL , 
+NgaySinh DATE NOT NULL , 
+Tuoi INT NOT NULL ,
+QueQuan VARCHAR(100) NOT NULL ,
+DiaChi VARCHAR(100) NOT NULL ,
+TrinhDo VARCHAR(100) NOT NULL ,
+SDT VARCHAR(11) NOT NULL , 
+Email VARCHAR(50) NOT NULL , 
+PRIMARY KEY (MaGV)) 
+
+--tk_gv
+CREATE TABLE tk_gv (UserName VARCHAR(30) NOT NULL , Password VARCHAR(30) NOT NULL , MaGV int, foreign key (MaGV) references giaovien(MaGV) , PRIMARY KEY (UserName));
+
+--lop
+CREATE TABLE lop 
+(MaLop varchar(20),
+TenLop VARCHAR(100) NOT NULL , 
+LuaTuoi int not null,
+ThoiGian date not null,
+ SLHS int not null DEFAULT '0',
+ SLHSToiDa int not null,
+ HocPhi float not null,
+ SoBuoi int not null,
+ SoBuoiDaToChuc int not null,
+ TrangThai varchar(200),
+PRIMARY KEY (MaLop)) 
+
+--gv_lop
+CREATE TABLE gv_lop
+(MAGV int,
+MaLop varchar(20),
+foreign key (MaGV) references giaovien(MaGV) ,
+foreign key (MaLop) references lop(MaLop) ,
+PRIMARY KEY (MaGV,MaLop)) 
+
+--hs_lop
+CREATE TABLE hs_lop 
+(MAHS int, 
+MaLop varchar(20),
+SoBuoiNghi int DEFAULT '0', 
+GiamHocPhi float DEFAULT '0', 
+foreign key (MaHS) references hocsinh(MaHS) , 
+foreign key (MaLop) references lop(MaLop) ,
+PRIMARY KEY (MaHS,MaLop));
+
+--diemdanh
+CREATE TABLE diemdanh
+(
+MaLop varchar(20),
+MAHS int, 
+ThoiGian date not null,
+dd boolean not null,
+foreign key (MaHS) references hocsinh(MaHS) , 
+foreign key (MaLop) references lop(MaLop) ,
+PRIMARY KEY (MaHS,MaLop,ThoiGian));
+
+--thuhocphi
+
+CREATE TABLE thuhocphi 
+(MaLop varchar(20) not null,
+ MAHS int NOT null,
+ ThoiGian date not null,
+ SoTien float NOT null,
+ GiamHocPhi float DEFAULT '0',
+ SoTienGiam float DEFAULT '0',
+ SoTienPhaiDong float not null,
+ SoTienDaDong float not null,
+ NoPhiConLai float not null,
+ TrangThai varchar(50),
+ NgayDong date ,
+PRIMARY KEY (MaHS,MaLop,ThoiGian)) 
+
 
 
 
